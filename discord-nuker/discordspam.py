fdca63207a9db99f76ee684da838a1d514dba08e
Inspecting <96089121+Inspecting@users.noreply.github.com>
2023-05-29 01:51:24 -0500

    Update discordspam.py

--- discord-nuker/discordspam.py
+++ discord-nuker/discordspam.py
@@ -11,19 +11,13 @@ with open("config.json") as f:
 # Set up logging
 logging.basicConfig(filename='logs.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-discord_settings = SETTINGS.get("discord", {})
-if discord_settings:
-    url = discord_settings.get("url")
-    authorization = discord_settings.get("authorization")
-    message = discord_settings.get("message")
-else:
-    print("Error: 'discord' section missing from config file.")
-    logging.error("'discord' section missing from config file.")
-    exit()
+channels = SETTINGS.get("channels", [])
+authorization = SETTINGS.get("authorization")
+message = SETTINGS.get("message")
 
-if not url or not authorization or not message:
-    print("Error: Discord 'url', 'authorization', and 'message' values must be specified in config file.")
-    logging.error("Discord 'url', 'authorization', and 'message' values must be specified in config file.")
+if not channels or not authorization or not message:
+    print("Error: 'channels', 'authorization', and 'message' values must be specified in config file.")
+    logging.error("'channels', 'authorization', and 'message' values must be specified in config file.")
     exit()
 
 delay = SETTINGS.get("delay", 1)  # Default delay time if no delay specified in config
@@ -32,33 +26,34 @@ message_count = 0  # Counter for number of messages sent
 
 try:
     while True:
-        payload = {
-            'content': message
-        }
+        for channel_url in channels:
+            payload = {
+                'content': message
+            }
 
-        r = requests.post(url, data=payload, headers={'authorization': authorization})
-        if r.ok:
-            success_message = f"Message successfully sent: {r.json()['content']}"
-            print(success_message)
-            logging.info(success_message)
-        else:
-            error_message = "Error sending message"
-            print(error_message)
-            logging.error(error_message)
+            r = requests.post(channel_url, data=payload, headers={'authorization': authorization})
+            if r.ok:
+                success_message = f"Message successfully sent to {channel_url}: {r.json()['content']}"
+                print(success_message)
+                logging.info(success_message)
+            else:
+                error_message = f"Error sending message to {channel_url}"
+                print(error_message)
+                logging.error(error_message)
 
-        message_count += 1
-        if message_count % 10 == 0:
-            os.system('cls' if os.name == 'nt' else 'clear')  # Clear console
+            message_count += 1
+            if message_count % 10 == 0:
+                os.system('cls' if os.name == 'nt' else 'clear')  # Clear console
 
-        if message_count % 100 == 0:
-            print(f"Pausing for cooldown ({cooldown_time} seconds remaining)...")
-            print("Paused due to too many requests.")
-            for i in range(cooldown_time, 0, -1):
-                print(f"\rResuming message sending in {i} seconds...", end="")
-                time.sleep(1)
-            print("\n") 
+            if message_count % 100 == 0:
+                print(f"Pausing for cooldown ({cooldown_time} seconds remaining)...")
+                print("Paused due to too many requests.")
+                for i in range(cooldown_time, 0, -1):
+                    print(f"\rResuming message sending in {i} seconds...", end="")
+                    time.sleep(1)
+                print("\n")
 
-        time.sleep(delay)  # Rate limit delay
+            time.sleep(delay)  # Rate limit delay
 
 except Exception as e:
     print(f"Encountered an error: {e}")
